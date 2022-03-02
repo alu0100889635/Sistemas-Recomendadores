be22b23b80ed8bba8c3d8c38b47db27ed5e6bd5c
Alba Cruz Torres <alu0100889635@ull.edu.es>
2022-03-02 10:50:05 +0000

    Se lee texto desde línea de comandos y se almacena en un array

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from importlib.resources import contents
 import sys
 
 #Se lee fichero por línea de comandos
 with open(sys.argv[1], 'r') as f:
-    contents = f.read()
+    contents = [line.rstrip() for line in f]
 print(contents)
