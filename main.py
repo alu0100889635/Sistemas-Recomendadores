2386495fc449d9d9bf83ceb4cf7f7cff7e963ac9
Alba Cruz Torres <alu0100889635@ull.edu.es>
2022-03-30 19:49:12 +0100

    Se calculan términos del documento

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import pandas as pd
 import re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
 #Se lee fichero por línea de comandos
 with open(sys.argv[1], 'r') as f:
@@ -8,14 +10,31 @@ with open(sys.argv[1], 'r') as f:
 
 foo = list(map(lambda x: re.split(r".\s", x, 1), contents))
 
-for i in contents:
-    x = re.split(r".\s", i, 1)
+prueba = []
 
-df = pd.DataFrame(foo, columns=['DocNumb', 'Document'])
+for i in foo:
+    prueba.append(i[1])
 
-df["Ind"] = " "
-df["Term"] = " "
-df["TF"] = " "
-df["TF-IDF"] = " "
-df["IDF"] = " "
-print(df)
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(prueba)
+word = vectorizer.get_feature_names()
+print(word)
+print(X.toarray())
+
+# transformer = TfidfTransformer()  
+# print(transformer)
+
+# tfidf = transformer.fit_transform(X)  
+# print(tfidf.toarray())
+
+
+
+#df = pd.DataFrame(foo, columns=['DocNumb', 'Document'])
+
+#df = pd.DataFrame(word, columns=['Term'])
+# df["Ind"] = " "
+# df["Term"] = " "
+# df["TF"] = " "
+# df["TF-IDF"] = " "
+# df["IDF"] = " "
+#print(df)
