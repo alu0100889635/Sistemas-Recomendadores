bce2127b92e2bc1a10c04c196c1cfecfb69c91e5
Alba Cruz Torres <alu0100889635@ull.edu.es>
2022-03-30 19:31:21 +0100

    Se separa el documento del número de documento y se crea el df

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from importlib.resources import contents
 import sys
+import pandas as pd
+import re
 
 #Se lee fichero por línea de comandos
 with open(sys.argv[1], 'r') as f:
     contents = [line.rstrip() for line in f]
-print(contents)
+
+foo = list(map(lambda x: re.split(r".\s", x, 1), contents))
+
+for i in contents:
+    x = re.split(r".\s", i, 1)
+
+df = pd.DataFrame(foo, columns=['DocNumb', 'Document'])
+
+df["Ind"] = " "
+df["Term"] = " "
+df["TF"] = " "
+df["TF-IDF"] = " "
+df["IDF"] = " "
+print(df)
