a582eff66ecebcabac9231bb9df129f2086c5593
Alba Cruz Torres <alu0100889635@ull.edu.es>
2022-04-01 12:21:54 +0100

    Se añaden comentarios aclaratorios

--- main.py
+++ main.py
@@ -10,14 +10,10 @@ with open(sys.argv[1], 'r') as f:
 
 foo = list(map(lambda x: re.split(r".\s", x, 1), contents))
 
-prueba = []
+test = []
 
 for i in foo:
-    prueba.append(i[1])
-
-test = []
-for x in prueba:
-    test.append([x])
+    test.append([i[1]])
 
 words = []
 tfidfdoc = []
@@ -25,15 +21,15 @@ for i in test:
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(i)
     word = vectorizer.get_feature_names_out()
+    #Se genera array con los términos de cada documento
     words.append(word)
     transformer = TfidfTransformer()
+    #Se calcula TF-IDF de cada término de cada documento y se almacena en otro array
     tfidf = transformer.fit_transform(X)
     tfidfdoc.append(tfidf.toarray())
 
-
-print(words)
-print(tfidfdoc)
-
+#Se genera dataframe donde cada línea representa un documento, se añade número de documento, el contenido del doc,
+#los términos de ese documento y el valor TF-IDF de cada término del documento.
 df = pd.DataFrame(foo, columns=['DocNumb', 'Document'])
 df['Terms'] = words
 df['TF-IDF'] = tfidfdoc
