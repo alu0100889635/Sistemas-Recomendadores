b559d12a810c623edbb267916dace2cd86dae432
Alba Cruz Torres <alu0100889635@ull.edu.es>
2022-04-01 13:30:09 +0100

    Se calcula tf-idf, índice del término

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 #Se lee fichero por línea de comandos
 with open(sys.argv[1], 'r') as f:
@@ -15,12 +16,19 @@ test = []
 for i in foo:
     test.append([i[1]])
 
+
 words = []
 tfidfdoc = []
+positions = []
+
 for i in test:
-    vectorizer = CountVectorizer()
+    vectorizer = CountVectorizer(stop_words = "english")
     X = vectorizer.fit_transform(i)
     word = vectorizer.get_feature_names_out()
+    position = []
+    for x in word:
+        position.append(i[0].lower().find(x))
+    positions.append(position)
     #Se genera array con los términos de cada documento
     words.append(word)
     transformer = TfidfTransformer()
@@ -31,6 +39,7 @@ for i in test:
 #Se genera dataframe donde cada línea representa un documento, se añade número de documento, el contenido del doc,
 #los términos de ese documento y el valor TF-IDF de cada término del documento.
 df = pd.DataFrame(foo, columns=['DocNumb', 'Document'])
+df['Term Ind'] = positions
 df['Terms'] = words
 df['TF-IDF'] = tfidfdoc
 
