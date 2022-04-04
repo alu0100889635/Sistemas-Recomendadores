31e4d8ec7344831b34637c3916aa4067cd706ec8
Alba Cruz Torres <alu0100889635@ull.edu.es>
2022-04-04 13:17:12 +0100

    Se añade lectura de otro fichero por consola y cálculo de similitud del coseno

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
-#Se lee fichero por línea de comandos
+#Se lee fichero de interests.txt por línea de comandos
 with open(sys.argv[1], 'r') as f:
-    contents = [line.rstrip() for line in f]
+    liked = [line.rstrip() for line in f]
 
-foo = list(map(lambda x: re.split(r".\s", x, 1), contents))
+#Se lee fichero de recomendar.txt por línea de comandos
+with open(sys.argv[2], 'r') as f:
+    recommend = [line.rstrip() for line in f]
 
+#Extraemos documentos que vamos a ver si podemos recomendar
+recomendations = list(map(lambda x: re.split(r".\s", x, 1), recommend))
+dfrec = pd.DataFrame(recomendations, columns=['DocNumb', 'Document'])
+
+#Extraemos documentos que sabemos que le han gustado al usuario
+interests = list(map(lambda x: re.split(r".\s", x, 1), liked))
 test = []
 
-for i in foo:
+for i in interests:
     test.append([i[1]])
 
-
+#Se calculan los vectores TF-IDF para cada término del documento
 words = []
 tfidfdoc = []
 positions = []
@@ -36,9 +44,15 @@ for i in test:
     tfidf = transformer.fit_transform(X)
     tfidfdoc.append(tfidf.toarray())
 
-df = pd.DataFrame(foo, columns=['DocNumb', 'Document'])
-df['Term Ind'] = positions
-df['Terms'] = words
-df['TF-IDF'] = tfidfdoc
+dfliked = pd.DataFrame(interests, columns=['DocNumb', 'Document'])
+dfliked['Term Ind'] = positions
+dfliked['Terms'] = words
+dfliked['TF-IDF'] = tfidfdoc
 
-print(df)
+print(dfliked)
+
+cosine_similarities = list(enumerate(map(cosine_similarity, dfliked['TF-IDF'])))
+# cosine_similarities = sorted(cosine_similarities, key = lambda x: x[1], reverse = True)
+# cosine_similarities = cosine_similarities[1:6]
+
+print(cosine_similarities)
