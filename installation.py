523509e585a0bb5b9a20f12ddf4d1d5c301b4be2
Alba Cruz Torres <alu0100889635@ull.edu.es>
2022-05-06 11:54:23 +0100

    Se añade un script para instalar los paquetes

---
+++ installation.py
@@ -0,0 +1,9 @@
+import sys
+import subprocess
+
+# implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'pandas'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', "-U", 
+'scikit-learn'])
